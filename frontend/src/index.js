8295bddfc726858ffd9eae9f9bd44df36b3e9e88
samyukthterawe <samyukthterawe@gmail.com>
2025-02-03 20:54:00 +0530

    Changed index.js for the websocket removal.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import express from 'express';
+import path from 'path';
+import userRoutes from './routes/userRoutes';
+import expenseRoutes from './routes/expenseRoutes';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+
+
+dotenv.config();
+connectDB();
+
+const server = express();
+server.use(express.json());
+server.use('/api/users', userRoutes);
+server.use('/api/expenses', expenseRoutes);
+
+
+server.use(express.static(path.join(__dirname, '../build')));
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
